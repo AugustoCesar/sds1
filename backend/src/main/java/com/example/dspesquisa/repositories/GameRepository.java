8d36b7e6a1e2deec1900d9ee37343ce3fac189fd
Augusto Cesar <augustocesar.udi@gmail.com>
2020-09-20 16:48:46 -0400

    DTO Service

--- backend/src/main/java/com/example/dspesquisa/repositories/GameRepository.java
+++ backend/src/main/java/com/example/dspesquisa/repositories/GameRepository.java
@@ -1,9 +1,11 @@
 package com.example.dspesquisa.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import com.example.dspesquisa.entities.Genre;
+import com.example.dspesquisa.entities.Game;
 
-public interface GameRepository extends JpaRepository<Genre, Long>{
+@Repository
+public interface GameRepository extends JpaRepository<Game, Long>{
 	
 }
